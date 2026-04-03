8d05cc5cb860fdb5478d7deed528c21c7c85bb61
Bright Munemu <brightmunemu@gmail.com>
2026-04-04 00:55:13 +0100

    add

--- db/controller/connection.php
+++ db/controller/connection.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 //defining the server, username, password and database
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u787607796_deluxechains'); 
-define('DB_PASSWORD', 'Deluxe14@');
+define('DB_USERNAME', 'u787607796_deluxechains');
+define('DB_PASSWORD', 'Deluxe1234@');
 define('DB_DATABASE', 'u787607796_deluxechains');
 //Create the connection to the database
 $connect = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
